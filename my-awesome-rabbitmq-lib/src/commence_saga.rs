4bf9648073240dcb1dcc12964b08a618062c0df2
jorge <jorgeclavijo@legendaryum.com>
2024-10-16 10:27:24 -0300

    feat: commence saga RankingsUsersReward

--- my-awesome-rabbitmq-lib/src/commence_saga.rs
+++ my-awesome-rabbitmq-lib/src/commence_saga.rs
@@ -12,10 +12,34 @@ use strum_macros::{AsRefStr, EnumIter, EnumString};
 #[serde(rename_all = "snake_case")]
 pub enum SagaTitle {
     PurchaseResourceFlow,
+    RankingsUsersReward,
 }
 
 pub trait PayloadCommenceSaga {
     fn saga_title(&self) -> SagaTitle;
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct UserReward {
+    pub user_id: String,
+    pub coins: i32,
+}
+impl PartialEq for UserReward {
+    fn eq(&self, other: &Self) -> bool {
+        self.user_id == other.user_id && self.coins == other.coins
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RankingsUsersRewardPayload {
+    pub rewards: Vec<UserReward>,
+}
+impl PayloadCommenceSaga for RankingsUsersRewardPayload {
+    fn saga_title(&self) -> SagaTitle {
+        SagaTitle::RankingsUsersReward
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -87,7 +111,10 @@ impl RabbitMQClient {
 
 #[cfg(test)]
 mod commence {
-    use crate::commence_saga::{CommenceSaga, PurchaseResourceFlowPayload, SagaTitle};
+    use crate::commence_saga::{
+        CommenceSaga, PurchaseResourceFlowPayload, RankingsUsersRewardPayload, SagaTitle,
+        UserReward,
+    };
     use crate::queue_consumer_props::Queue;
     use crate::test::setup::TestSetup;
     use futures_lite::StreamExt;
@@ -139,6 +166,55 @@ mod commence {
             assert_eq!(
                 received_message.payload.user_id, user_id,
                 "Received message should match sent message"
+            );
+        });
+    }
+
+    #[test]
+    fn test_commence_rankings_users_reward_saga() {
+        let setup = TestSetup::new(None);
+        setup.rt.block_on(async {
+            let rewards = vec![
+                UserReward {
+                    user_id: "user123".to_string(),
+                    coins: 100,
+                },
+                UserReward {
+                    user_id: "user456".to_string(),
+                    coins: 200,
+                },
+            ];
+
+            let payload = RankingsUsersRewardPayload {
+                rewards: rewards.clone(),
+            };
+
+            setup.client.commence_saga(payload).await.unwrap();
+
+            // The transactional microservice receives the message and processes it
+            let mut consumer = setup
+                .client
+                .consume_messages::<CommenceSaga<RankingsUsersRewardPayload>>(
+                    Queue::COMMENCE_SAGA,
+                    BasicConsumeOptions::default(),
+                )
+                .await
+                .expect("Failed to create consumer");
+
+            let received_message = tokio::time::timeout(Duration::from_secs(2), consumer.next())
+                .await
+                .expect("Timed out waiting for message")
+                .expect("Failed to receive message")
+                .expect("Error in received message");
+
+            assert_eq!(
+                received_message.title,
+                SagaTitle::RankingsUsersReward,
+                "Received saga title should match sent title"
+            );
+            assert_eq!(
+                received_message.payload.rewards, rewards,
+                "Received message rewards should match sent rewards"
             );
         });
     }
